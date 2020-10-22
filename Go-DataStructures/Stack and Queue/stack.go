01c06c1d91b2a2f00e471e6fde48f3a914a88427
Priyanshi Panchal <72795372+PriyanshiPanchal@users.noreply.github.com>
2020-10-22 23:27:27 +0530

    Add files via upload

---
+++ Go-DataStructures/Stack and Queue/stack.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Stack struct {
+	items []int
+}
+
+
+func (s *Stack) Push(i int) {
+	s.items = append(s.items, i)
+}
+
+func (s *Stack) Pop() int {
+	lastIndex := len(s.items) - 1
+	toRemove := s.items[lastIndex]
+	s.items = s.items[:lastIndex]
+	return toRemove
+}
+
+func main() {
+	myStack := Stack{}
+	fmt.Println(myStack)
+	myStack.Push(35)
+	myStack.Push(23)
+	myStack.Push(78)
+	fmt.Println(myStack)
+	myStack.Pop()
+	fmt.Println(myStack)
+}
